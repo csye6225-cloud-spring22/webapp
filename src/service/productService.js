4324837d08420559d277894f4f2a52c8392fbd53
namrata_ruchandani_002125637 <ruchandani.n@northeastern.edu>
2023-02-08 18:44:26 -0500

    Updated some error handling

--- src/service/productService.js
+++ src/service/productService.js
@@ -140,17 +140,6 @@ const product_create = async (body,id) => {
         "Can't update the following fields: owner_user_id, account_created or account_updated"
       );
     }
-    if (
-      name == undefined ||
-      sku == undefined ||
-      manufacturer == undefined ||
-      description == undefined ||
-      quantity == undefined 
-    ) {
-      throw new BadRequestException(
-        "Can't update the following fields: owner_user_id, account_created or account_updated"
-      );
-    }
   
     try {
       const response = await Product.update(
