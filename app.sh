bf38c34af900ff52ab170bdc74894afebf53e6ef
namrata_ruchandani_002125637 <ruchandani.n@northeastern.edu>
2023-02-20 01:05:03 -0500

    Adding assignment4 AMI

---
+++ app.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+sleep 30
+
+# touch ~/.bash_profile
+
+# echo -e "export DB_NAME=DEV\nexport DB_USER=root\nexport DB_PASSWORD=PaSswo#2\nexport DB_HOST=localhost\nexport PORT=8000" > ~/.bash_profile
+
+# source ~/.bash_profile
+
+touch ~/.bash_profile
+echo -e "export DB_DATABASE=USER\nexport DB_USERNAME=root\nexport DB_PASSWORD=PaSswo#2\nexport DB_HOST=localhost\nexport PORT=8000" > ~/.bash_profile
+source ~/.bash_profile
+
+sudo yum update -y
+
+# sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+
+sudo amazon-linux-extras install epel -y 
+sudo yum install https://dev.mysql.com/get/mysql80-community-release-el7-5.noarch.rpm -y
+sudo yum install mysql-community-server -y
+sudo systemctl start mysqld.service
+
+# export PORT=8000
+
+# export DB_HOST="localhost"
+# export DB_USER="root"
+# export DB_NAME="DEV"
+# export DB_PASSWORD="sUckS&23"
+
+export temp=$(sudo cat /var/log/mysqld.log | grep "A temporary password" | awk -F ' ' '{print $NF}')
+sudo mysql -u root -p$temp --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'PaSswo#2';CREATE DATABASE USER;USE USER;"
+
+cd /home/ec2-user && unzip ./webapp.zip
+
+
+npm install
+
+# sudo yum install unzip -y
+# cd ~/ && unzip cocktails.zip
+# cd ~/cocktails && npm i --only=prod
+
+# sudo mv /tmp/cocktails.service /etc/systemd/system/cocktails.service
+# sudo systemctl enable cocktails.service
+# sudo systemctl start cocktails.service
---
+++ poc.pkr.hcl
@@ -0,0 +1,54 @@
+packer {
+  required_plugins {
+    amazon = {
+      version = ">= 0.0.2"
+      source  = "github.com/hashicorp/amazon"
+    }
+  }
+}
+locals {
+  timestamp = regex_replace(timestamp(), "[- TZ:]", "")
+}
+
+
+source "amazon-ebs" "trial" {
+  profile = "dev"  
+  ami_name = "Customized_AMI-${local.timestamp}"
+
+  source_ami_filter {
+    filters = {
+      name                = "amzn2-ami-kernel-5.10-hvm-2.0.20230207.0-x86_64-gp2"
+      root-device-type    = "ebs"
+      virtualization-type = "hvm"
+    }
+    most_recent = true
+
+    owners = ["amazon"]
+  }
+//   ami_name   = "trial"
+//   source_ami = "ami-01133559d62203fd3"
+
+
+  instance_type = "t2.micro"
+  region        = "us-east-1"
+  ssh_username  = "ec2-user"
+}
+
+build {
+  sources = [
+    "source.amazon-ebs.trial"
+  ]
+  provisioner "file" {
+    source      = "./webapp.zip"
+    destination = "/home/ec2-user/webapp.zip"
+  }
+
+  //   provisioner "file" {
+  //     source = "./trial.service"
+  //     destination = "/tmp/trial.service"
+  //   }
+
+  provisioner "shell" {
+    script = "./app.sh"
+  }
+}
